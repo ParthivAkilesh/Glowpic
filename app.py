fc0d1ab11ac1bfae7ebd741bd4961a79b3c672ee
Parthiv Akilesh <71423865+ParthivAkilesh@users.noreply.github.com>
2022-12-26 12:49:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.markdown('<h1 style="color:white;">Image Enhancement Model</h1>', unsafe_allo
 st.markdown('<h2 style="color:white;">This uses opencv to enhance the image given by extracting important features</h2>', unsafe_allow_html=True)
 
 
-# background image to streamlit
+
 
 @st.cache(allow_output_mutation=True)
 def get_base64_of_bin_file(bin_file):
@@ -19,23 +19,7 @@ def get_base64_of_bin_file(bin_file):
         data = f.read()
     return base64.b64encode(data).decode()
 
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file) 
-    page_bg_img = '''
-    <style>
-    .stApp {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-attachment: scroll; # doesn't work
-    }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
 
-set_png_as_page_bg('P:\\Police Hackathon\\Glowpic\\content\\bg2.webp')
 
 upload= st.file_uploader('Insert image for classification', type=['png','jpg'])
 c1, c2= st.columns(2)
